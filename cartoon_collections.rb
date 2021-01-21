22e9bb41813b37d906c9c632d0b80364af76bb0b
Nick Gray <nickgray87@gmail.com>
2021-01-21 23:55:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,4 +24,5 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.find do
     planeteer_calls == valid_calls
+  end
 end
